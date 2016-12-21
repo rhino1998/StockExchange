5966b12af4b7599f2d8127f438fbfb71a9753e2b
rhino1998 <rhino1998@gmail.com>
2016-12-21 15:19:11 -0500

    Accounts sig

--- src/main/java/com/stockexchange/traders/accounts/Account.java
+++ src/main/java/com/stockexchange/traders/accounts/Account.java
@@ -6,6 +6,19 @@ public class Account {
 	
 	
 	private final String name;
+	private final Trader owner;
 	private int balance;
+	
+	
+	public Account(String name, Trader owner)
+	public Account(String name, Trader owner, int money)
+	
+	public int deposit(int money){
+		
+	}
+	
+	public int withdraw(int money){
+		
+	}
 
 }
