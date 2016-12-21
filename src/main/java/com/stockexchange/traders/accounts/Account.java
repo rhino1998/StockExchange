0c1651855f11843a5f853567ebc67d7d4ce9694a
coleschn <coleschn@gmail.com>
2016-12-21 15:27:42 -0500

    Account

--- src/main/java/com/stockexchange/traders/accounts/Account.java
+++ src/main/java/com/stockexchange/traders/accounts/Account.java
@@ -12,15 +12,26 @@ public class Account {
 	private int balance;
 	
 	
-	public Account(String name, Trader owner)
-	public Account(String name, Trader owner, int money)
+	public Account(String name, Trader owner){
+		this.name = name;
+		this.owner = owner;
+	}
+
+	public Account(String name, Trader owner, int money){
+		this.name = name;
+		this.owner = owner;
+		this.balance = money;
+	}
+	
 	
 	public int deposit(int money){
-		
+		balance += money;
+		return balance;
 	}
 	
 	public int withdraw(int money){
-		
+		balance -= money;
+		return balance;
 	}
 
 }
