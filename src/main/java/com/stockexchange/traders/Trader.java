5dd2d6db8579be15d88cfbcbbcdee5f4057bd765
gmguzman <gianmarco.m.guzman@gmail.com>
2016-12-21 14:56:10 -0500

    updated Trader

--- src/main/java/com/stockexchange/traders/Trader.java
+++ src/main/java/com/stockexchange/traders/Trader.java
@@ -5,38 +5,71 @@ import java.util.UUID;
 
 import org.glassfish.jersey.message.internal.Token;
 
+import StockExchange.traders.Brokerage;
+import StockExchange.traders.Trader;
+
 import com.stockexchange.stocks.orders.Order;
 import com.stockexchange.stocks.orders.OrderView;
 import com.stockexchange.traders.accounts.Account;
 import com.stockexchange.transport.enums.State;
 
-public class Trader {
+public class Trader implements Comparable{
 
 	
 	private final String username;
 	private String name;
+	private String pswd;
 	private HashMap<String,Account> accounts;
 	private HashMap<UUID, Order> pendingOrders;
+	private Brokerage brokerage;
+	
 	
 	
 	private transient State state;
 	private transient Token token;
 	
-	
+	public Trader(Brokerage b, String n, String p){
+		brokerage = b;
+		name = n.toUpperCase();
+		pswd = p;
+	}
 	
 	public String getUsername(){
-		//TODO write this;
+		return this.username;
 	}
 	
 	public String getName(){
-		//TODO write this;
+		return this.name;
 	}
 	
 	public OrderView getPendingOrderView();
 	public void submitOrder(Order order);
+	
+	public boolean equals(Object other){
+		try{
+			if(this.compareTo((Trader) other)==0){
+				return true;
+			}else{
+				return false;
+			}
+		}catch(ClassCastException e){
+			System.out.println("ERROR");
+			return false;
+		}
+	}
 
 	public Password getPassword() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public int compareTo(Object arg0) {
+		try{
+			int ans = this.compareTo((Trader)arg0);
+			return ans;
+		}catch(ClassCastException e){
+			System.out.println("ERROR");
+			return 123456789;
+		}
 	}
 }
